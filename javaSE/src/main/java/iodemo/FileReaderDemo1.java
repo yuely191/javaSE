85e062bcd9618fb503b438db8dc4cbfc596adeb3
yuely191 <yuely191@qq.com>
2018-05-14 18:30:42 +0800

    add some classDemo

--- javaSE/src/main/java/iodemo/FileReaderDemo1.java
+++ javaSE/src/main/java/iodemo/FileReaderDemo1.java
@@ -7,12 +7,15 @@ public class FileReaderDemo1 {
 	
 	public static void main(String[] args) {
 		FileReader fr=null;
-		char[] ch=new char[1024];
+		char[] ch=new char[5];
 		int len;
 		try {
 			fr=new FileReader("demo.txt");
 			while(-1 !=(len=fr.read(ch))) {
-				System.out.println(new String(ch,0,len));
+				System.out.print(len);
+				System.out.print(new String(ch,0,len));
+				
+				
 			}
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
